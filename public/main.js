955d971e4f6c7c8d9a2d5b523876cf7aadb954d2
Jonathan Troy <jonrtroy@gmail.com>
2017-02-21 13:52:21 -0500

    updated js and css

--- public/main.js
+++ public/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+  $('#new-form').on('submit', (event) => {
+    if($('input[name="topics[title]"]').val() === '' || $('textarea[name="topics[content]"]').val() === '' ) {
+      alert('Oops, please fill out all parts of the form');
+      return false;
+    }
+  });
 
+  $('#show-form').on('submit', (event) => {
+    if($('textarea[name="comments[content]"]').val() === '') {
+      alert('Oops, you did not use your words!');
+      return false;
+    }
+  });
 
 });
